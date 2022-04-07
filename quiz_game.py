09cb63e960a1d7adf49f4292cf93386491213736
aharrah95 <abigail.harrah@revature.net>
2022-04-07 16:31:23 -0500

    10 questions, 4/7/22

--- quiz_game.py
+++ quiz_game.py
@@ -29,3 +29,61 @@ if answer == "1885":
     print("Correct!")
 else:
     print("Incorrect!")
+
+print("")
+
+answer = input("Question 4. What band duo performed the hit songs 'Stressed Out', and 'Heathens'? ")
+if answer == "twenty one pilots":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+print("")
+
+answer = input("Question 5. Who is the creator behind the massively popular game 'Five Nights at Freddy's''? ")
+if answer == "Scott Cawthon":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+print("")
+
+answer = input("Question 6. What home game console had the number '64' in it's title, predating the Nintendo 64? ")
+if answer == "Commodore 64":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+print("")
+
+answer = input("Question 7. True or false: tomatoes are a vegetable? ")
+if answer == "False":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+print("")
+
+answer = input("Question 8. What movie ended with the infamous 'man in the woodchipper', and later became a series on "
+               "FX? ")
+if answer == "Fargo":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+print("")
+
+answer = input("Question 9. What Coen Brothers movie has a restaurant based on it in Cedar Rapids, IA? ")
+if answer == "The Big Lebowski":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+print("")
+
+answer = input("Question 10. True or false: Chica will let you feed her pizza slices in Five Nights at Freddy's VR's "
+               "'Parts & Services'' minigame? ")
+if answer == "True":
+    print("Correct!")
+else:
+    print("Incorrect!")
