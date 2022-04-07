60b22de5fac984024cec5adfa2b51eeb34032f78
aharrah95 <abigail.harrah@revature.net>
2022-04-07 16:51:02 -0500

    All questions added, scoring system implemented. FINAL 4/7/22

--- quiz_game.py
+++ quiz_game.py
@@ -7,10 +7,12 @@ if playing != "Yes":
     quit()
 
 print("Excellent! Let's begin!")
+score = 0
 
 answer = input("Question 1. Boris from 'Bendy and the Ink Machine' is an anthropomorphic member of what species? ")
 if answer == "Wolf":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
@@ -19,6 +21,7 @@ print("")
 answer = input("Question 2. Is Gyarados a Dragon-type Pokemon? ")
 if answer == "No":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
@@ -27,6 +30,7 @@ print("")
 answer = input("Question 3. What year was the University of St. Thomas (Minnesota) founded? ")
 if answer == "1885":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
@@ -35,6 +39,7 @@ print("")
 answer = input("Question 4. What band duo performed the hit songs 'Stressed Out', and 'Heathens'? ")
 if answer == "twenty one pilots":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
@@ -43,6 +48,7 @@ print("")
 answer = input("Question 5. Who is the creator behind the massively popular game 'Five Nights at Freddy's''? ")
 if answer == "Scott Cawthon":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
@@ -51,6 +57,7 @@ print("")
 answer = input("Question 6. What home game console had the number '64' in it's title, predating the Nintendo 64? ")
 if answer == "Commodore 64":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
@@ -59,6 +66,7 @@ print("")
 answer = input("Question 7. True or false: tomatoes are a vegetable? ")
 if answer == "False":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
@@ -68,6 +76,7 @@ answer = input("Question 8. What movie ended with the infamous 'man in the woodc
                "FX? ")
 if answer == "Fargo":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
@@ -76,6 +85,7 @@ print("")
 answer = input("Question 9. What Coen Brothers movie has a restaurant based on it in Cedar Rapids, IA? ")
 if answer == "The Big Lebowski":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
@@ -85,5 +95,8 @@ answer = input("Question 10. True or false: Chica will let you feed her pizza sl
                "'Parts & Services'' minigame? ")
 if answer == "True":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
+print("You got " + str(score) + " questions correct!")
+print("You got " + str((score / 10) * 100) + "%!")
